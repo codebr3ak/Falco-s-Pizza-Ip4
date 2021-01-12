9af185acc3ca8d91a607a1abe3fe8329bda03dc7
codebr3ak <barakaralph@gmail.com>
2021-01-12 23:19:45 +0300

    New changes made

--- javascript/main.js
+++ javascript/main.js
@@ -1,122 +1,53 @@
-let price;
-let crust_price;
-let topping_price;
-let total = 0
+var Order=function(size,crust,topping){
+  this.name=name;
+  this.size=size;
+  this.crust=crust;
+  this.topping=topping
 
-function GetPizza(name, size, crust, topping,total){
-    this.name = name;
-    this.size = size;
-    this.crust = crust;
-    this.topping = topping;
-    this.total = total;
-};
-
-$(document).ready(function(){
-    $(button.checkout).click(function(event){
-        let pizzaName = $("#name option:selected").val();
-        let pizzaSize = $("#size option:selected").val();
-        let pizzaCrust = $("#crust option:selected").val();
-        let pizzaTopping = [];
-    })
+  
+  Order.prototype.Tprice= function()
+  {
+  return this.size + this.crust + this.topping
+  }
+  
 }
 
-.each($("input[name='toppings']:checked"),function(){
-    pizzaTopping.push($(this).val());
+$("document").ready(function(){
+  var total=0;
+  $("button.book").click(function(event){
+      event.preventDefault();
+     var pSize=parseInt($("#size option:selected").val()); 
+     var pCrust =parseInt($("#crust option:selected").val()); 
+     var pTopping= parseInt($("#topping option:selected").val());
+    
+     
+    
+
+  
+
+  let nOrder = new Order(pSize,pCrust,pTopping);
+
+  let price= (nOrder.Tprice());
+
+  total+=price;
+
+    var Tsize= $("#size option:selected").text();
+    var Tcrust= $("#crust option:selected").text();
+    var Ttopping= $("#topping option:selected").text();
+    var Tname= $("#name option:selected").text();
+
+    $("tbody:last").append("<tr>"+"<td>"+Tname+"</td>"+"<td>"+Tsize+"</td>"+"<td>" +Tcrust+ "</td>" +"<td>"+Ttopping+ "</td>" + "<td>" +price +"</td>"+"</tr>")
+    
+  });
+
+  $("button#final").click(function(event){
+      $("#price:last").text(total);
+      $("#last").show();
+      
+          alert("thanks " + nName + " for your add and you could make some more if you want, this will be delivered in " + nlocation+ " on time")
+    
+
   })
-
-
-  switch(pizzaSize){
-    case "0":
-      price= 0;
-      break;
-    case 'large':
-      price = 1200;
-      break;
-    case "medium":
-      price = 800;
-      break;
-      case "regular":
-      price = 600;
-      break;
-      break;
-    case "small":
-      price = 300;
-    default:
-      console.log("error");
-  }
-  switch(pizzaCrust){
-    case "0":
-      c_price = 0;
-      break;
-    case "thin":
-      c_price = 100;
-      break;
-    case "thick":
-      c_price = 150;
-      break;
-    case "flatbread":
-      c_price = 200;
-      break;
-    case "custom":
-      c_price = 250;
-    default:
-      console.log("error");
-  }
-
-  let topping_value = pizzaTopping.length * 100;
-
-  if((pizzaSize == "0") && (pizzaCrust == "0")){
-    $("button.checkout").show();
-    $("#summary").show();
-    $("div.choice").hide();
-    alert("Please select Pizza size and crust");
-  }
-
-  else {
-    $("button.checkout").hide();
-    $("#summary").hide();
-    $("div.choice").slideDown(1200);
-  }
-
-  function getSizeValue() {
-    var selectedValue = document.getElementById("size").value;
-    return parseInt(selectedValue);
-}
-function getCrust() {
-    var selectedCrust = document.getElementById("crust").value;
-    return parseInt(selectedCrust);
-}
-function getToppings() {
-    var selectedToppings = document.getElementById("toppings").value;
-    return parseInt(selectedToppings);
-}
-function getQuantity() {
-    var selectedQuantity = document.getElementById("quan").value;
-    return parseInt(selectedQuantity);
-}
-
-function getTotalAmount() {
-    var totalAmount = (getSizeValue() + getCrust() + getToppings()) * get();
-    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
-    prompt("enter your location")
-    alert("You delivery fee is 150/=")
-    alert("You order will be delivered shortly")
-    alert("Continue shoppping with us each and every time .")
-}
-
-
-
-// Delivery 
-$(".deliver").click(function () {
-    $('.feedback').slideUp();
-    $('#list').slideUp();
-    $('.feedback').text("Provide location details").slideDown();
-    $('.deliver').hide(1000);
-    $('.delivernot').hide(1000);
-    $('.cdata-overlay').slideDown();
 });
 
-//Pick Up
-$(".delivernot").click(function () {
-
-});
+  4
