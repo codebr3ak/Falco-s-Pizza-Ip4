6cae4767608d1838108325ac6423db9035862348
codebr3ak <barakaralph@gmail.com>
2021-01-12 09:21:09 +0300

    new files added

--- javascript/main.js
+++ javascript/main.js
@@ -78,9 +78,31 @@ $(document).ready(function(){
     $("div.choice").slideDown(1200);
   }
 
-  total = price + c_price + topping_value;
-  let checkoutTotal = 0;
-  checkoutTotal = checkoutTotal + total;
+  function getSizeValue() {
+    var selectedValue = document.getElementById("size").value;
+    return parseInt(selectedValue);
+}
+function getCrust() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getToppings() {
+    var selectedToppings = document.getElementById("toppings").value;
+    return parseInt(selectedToppings);
+}
+function getQuantity() {
+    var selectedQuantity = document.getElementById("quan").value;
+    return parseInt(selectedQuantity);
+}
+
+function getTotalAmount() {
+    var totalAmount = (getSizeValue() + getCrust() + getToppings()) * get();
+    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+    prompt("enter your location")
+    alert("You delivery fee is 150/=")
+    alert("You order will be delivered shortly")
+    alert("Continue shoppping with us each and every time .")
+}
 
 
 
